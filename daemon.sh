dde2de777c52e961c4997230402ac00c7b65a9d9
Lorenzo Silvestri <code@silvestrix.org>
2018-02-03 19:47:29 +0000

    use multithreaded flask

--- daemon.sh
+++ daemon.sh
@@ -1,1 +1,1 @@
-sudo -u lorenzo screen -S flask -d -m bash -c "export FLASK_APP=backend.py; python -m flask run --host=0.0.0.0 --port=50500"
+sudo -u lorenzo screen -S flask -d -m bash -c "export FLASK_APP=backend.py; python -m flask run --host=0.0.0.0 --port=50500 --with-threads"
